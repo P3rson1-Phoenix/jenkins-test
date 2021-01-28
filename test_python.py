63d589fa770b043980cd23198b96beb43618189c
P3rson1-Phoenix <amritha.anmol@gmail.com>
2021-01-28 14:06:09 +0530

    User's input with space considered

--- test_python.py
+++ test_python.py
@@ -2,7 +2,7 @@ import time
 print("This is first test")
 
 
-var = input("Enter the url you want to download")
+var = input("Enter the url you want to download: ")
 print()
 print("The url you have mentioned is : {} ".format(var))
 
