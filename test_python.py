5c0b8ea6d65f9673abb997250e4c8f2b1f417cdc
P3rson1-Phoenix <amritha.anmol@gmail.com>
2021-01-28 14:02:19 +0530

    User's input considered

--- test_python.py
+++ test_python.py
@@ -1,6 +1,12 @@
 import time
 print("This is first test")
 
+
+var = input("Enter the url you want to download")
+print()
+print("The url you have mentioned is : {} ".format(var))
+
+
 print()
 for i in range (10):
     print("THis ius test 2")
